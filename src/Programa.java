dd8b63645c9715ae7743d63d2189e6cd0b5f4b06
PominiGa <gabriel.souza53@hotmail.com>
2026-09-07 10:11:27 -0300

    refactor: move IMC calculation logic to a separate method

--- src/Programa.java
+++ src/Programa.java
@@ -1,6 +1,23 @@
 import java.util.Scanner;
 
 public class Programa {
+    public void calcularIMC(double peso, double altura) {
+        double imc = peso / (altura * altura);
+        String imcFormatado = String.format("%.2f", imc);
+        System.out.println("Seu imc é: " + imcFormatado);
+
+        if (imc < 17) {
+            System.out.println("Muito abaixo do peso");
+        } else if (imc >= 17 && imc < 18.49) {
+            System.out.println("Abaixo do peso");
+        } else if (imc >= 18.50 && imc < 24.99) {
+            System.out.println("Peso normal");
+        } else if (imc >= 25 && imc < 30) {
+            System.out.println("Acima do peso");
+        } else {
+            System.out.println("Obesidade");
+        }
+    }
     public static void main(String[] args) {
 
         System.out.print("Digite seu peso: ");
@@ -10,25 +27,7 @@ public class Programa {
         System.out.print("Digite sua altura: ");
         double altura = scanner.nextDouble();
 
-        double imc = peso / (altura * altura);
-
-        String imcFormatado = String.format("%.2f", imc);
-
-        System.out.println("Seu imc é: "+ imcFormatado);
-
-        if (imc < 17){
-            System.out.println("Muito abaixo do peso");
-        }
-        if (imc > 17 && imc < 18.49){
-            System.out.println("Abaixo do peso");
-        }
-        if (imc > 18.50 && imc < 24.99){
-            System.out.println("Peso normal");
-        }
-        if (imc >25 && imc < 30){
-            System.out.println("Acima do peso");
-        }
-
-
+        Programa programa = new Programa();
+        programa.calcularIMC(peso, altura);
     }
 }
